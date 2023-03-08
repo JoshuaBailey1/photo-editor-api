28fe06290446ba9b208a29c5b7fc92d8d76b5f04
Josh Bailey <josh_bailey@sweetwater.com>
2023-03-07 21:54:22 -0500

    fix: request validation

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import * as bodyParser from 'body-parser';
 import { AppModule } from './app.module';
@@ -6,6 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(bodyParser.json({ limit: '50mb' }));
   app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
   await app.listen(8080);
 }
 bootstrap();
